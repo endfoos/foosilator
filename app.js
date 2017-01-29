5e27355f4e7fc39f7592da6131493a7145c894ef
Lewis Christie <lewis.christie@gmail.com>
2017-01-30 11:29:09 +1300

    add sessions

--- app.js
+++ app.js
@@ -112,6 +112,65 @@ const hbs = require('hbs')
 
 // Setup Express
 const app = express()
+const helmet = require('helmet')
+app.use(helmet())
+const session = require('express-session')
+const cookieSettings = {
+  httpOnly: true,
+  secure: NODE_ENV !== 'development',
+  maxAge: 14 * 86400000 // 14 days
+}
+class PgPromiseSessionStore extends session.Store {
+  destroy (sid, callback) {
+    db.none('DELETE FROM session WHERE sid=$1', [sid])
+    .then(callback)
+    .catch(callback)
+  }
+  get (sid, callback) {
+    db.any('SELECT data FROM session WHERE sid=$1', [sid])
+    .then((results) => {
+      if (results[0]) {
+        callback(null, results[0].data)
+      } else {
+        callback()
+      }
+    })
+    .catch(callback)
+  }
+  set (sid, session, callback) {
+    // Clean up old sessions
+    db.none(`
+      DELETE FROM session WHERE expires_at < NOW() - INTERVAL '1 minute'
+    `)
+    .then(() => {
+      return db.none(`
+        INSERT INTO session(sid, data, expires_at)
+        VALUES($1, $2, $3)
+        ON CONFLICT (sid) DO UPDATE
+        SET data=$2, expires_at=$3
+        `,
+        [sid, session, session.cookie._expires]
+      )
+    })
+    .then(callback)
+    .catch(callback)
+  }
+  touch (sid, session, callback) {
+    db.none(
+      'UPDATE session SET expires_at=$2 WHERE sid=$1',
+      [sid, session.cookie._expires]
+    )
+    .then(callback)
+    .catch(callback)
+  }
+}
+app.use(session({
+  secret: 'q4IDJtrvnfCPUSVx9OxdQrufur6whFhD7rGLNrlY',
+  cookie: cookieSettings,
+  resave: false,
+  saveUninitialized: false,
+  store: new PgPromiseSessionStore()
+}))
 app.use(bodyParser.urlencoded({
   extended: true
 }))
